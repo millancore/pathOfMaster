357d2178a8702085fbfa3d8d5c24cdb8292a154a
lehvui <serlen159@gmail.com>
2018-11-02 14:55:36 -0500

    error 0.0.1

--- ui/js/treeskills.js
+++ ui/js/treeskills.js
@@ -1,12 +1,43 @@
 
-var login = function() {
+var login = function () {
   axios.get('templates/login.hbs').then(function (response) {
     document.getElementById('render').innerHTML = response.data;
   })
-}
+};
 
- var routes = {
+var formnode = function () {
+  axios.get('templates/formnode.hbs').then(function (response) {
+    document.getElementById('render').innerHTML = response.data;
+  })
+};
+
+var formtree = function () {
+  axios.get('templates/formtree.hbs').then(function (response) {
+    document.getElementById('render').innerHTML = response.data;
+  })
+};
+
+var home = function () {
+  axios.get('templates/home.hbs').then(function (response) {
+    document.getElementById('render').innerHTML = response.data;
+  })
+};
+
+var tree = function () {
+  axios.get('templates/tree.hbs').then(function (response) {
+    document.getElementById('render').innerHTML = response.data;
+  })
+};
+
+var routes = {
   '/': login,
+  '/formnode': formnode,
+  '/formtree': formtree,
+  '/home': home,
+  '/tree': tree,
+
+
+
 };
 
 var router = Router(routes);
