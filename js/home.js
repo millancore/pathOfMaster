8ddacc004a65c9e9fd0ffcfb54e68317ef4924fc
lehvui <serlen159@gmail.com>
2018-11-19 07:52:43 -0500

    homeTree listener

--- js/home.js
+++ js/home.js
@@ -2,25 +2,37 @@ const loadTemplate = require('./templateLoader');
 
 var home = function () {
     loadTemplate('layout', 'render').then(() =>
-        loadTemplate('home', 'container').then(() =>{
+        loadTemplate('home', 'container').then(() => {
             homeTree();
             console.log("jajajajajaja")
 
         }
-        
+
         )
     )
 };
 
 
-function homeTree(){
+function homeTree() {
     var treArray = JSON.parse(localStorage.getItem("usuarioarbol"));
-    for (x=0;x<treArray.length;x++){
+    for (x = 0; x < treArray.length; x++) {
         var para = document.createElement("p");
+        para.setAttribute("id", treArray[x]);
+
         var node = document.createTextNode(treArray[x]);
         para.appendChild(node);
-         var element = document.getElementById("desss");
+        var element = document.getElementById("desss");
         element.appendChild(para);
+
+        para.addEventListener('onclick', function (event) {
+            event.preventDefault();
+            console.log("holaaa")
+            window.location.assign('#/tree/' + treArray[x]);
+
+        }, false);
+
+
+
     };
 
 
