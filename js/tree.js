e5814d81b1dc78b6da269494374701417b89b124
lehvui <serlen159@gmail.com>
2018-11-28 08:33:08 -0500

    modification of function printtree svg

--- js/tree.js
+++ js/tree.js
@@ -52,47 +52,50 @@ function treeListener() {
 
 function printtree(treeName){
     var nodeArray = JSON.parse(localStorage.getItem(treeName+"-node"));
+    var nodos = SVG("tree-see").size(300, 10000);
+    var circley = 50;
+    var liney1 = 60;
+    var liney2 = 0;
 
-    var myTemplateConfig = {
-        colors: [ "#F00", "#0F0", "#00F" ], // branches colors, 1 per column
-        branch: {
-          lineWidth: 8,
-          spacingX: 50,
-          showLabel: true,                  // display branch names on graph
-        },
-        commit: {
-          spacingY: -80,
-          dot: {
-            size: 12
-          },
-          message: {
-            displayAuthor: true,
-            displayBranch: false,
-            displayHash: false,
-            font: "normal 12pt Arial"
-          },
-          shouldDisplayTooltipsInCompactMode: false, // default = true
-          tooltipHTMLFormatter: function ( commit ) {
-            return "" + commit.sha1 + "" + ": " + commit.message;
-          }
-        }
-      };
-      var myTemplate = new GitGraph.Template( myTemplateConfig );
+    for (x = 0; x < nodeArray.length; x++) {
+        liney2 += 90; 
+    }
 
-      var gitgraph = new GitGraph({
-        template: myTemplate, 
-        orientation: "vertical",
-        author: '',
-        mode: "extended" 
-    });
-  
-    var master = gitgraph.branch("master");
-  
-    for (x=0;x<nodeArray.length;x++){
-        gitgraph.commit(nodeArray[x].name);
-    };
-  
-  }
+    var line = nodos.line(133, liney1, 133, liney2).stroke({ width: 10 });
+    
+    line.fill( '#f09' )
+
+    for (x = 0; x < nodeArray.length; x++) {
+         var circle = nodos.circle(65).fill('#4e6fc9').move(100, circley);
+
+    
+    
+    
+            
+        liney1 += 190;
+        circley += 90;
+
+        let i = nodeArray[x]
+        circle.click(function() {
+            axios.get('templates/nodeDescription.hbs').then(
+                function (response) {
+                    var template = Handlebars.compile(response.data);
+                    var dataTree = JSON.parse(localStorage.getItem(treeName+"-node"));
+                    var nodeName = i.name;
+                    var nodeDescription = i.descripcion;
+                    var node = {name: nodeName,description: nodeDescription };
+                    console.log(node)
+
+                    document.getElementById('content').innerHTML = template(node);
+                }
+            );
+        })
+    }    
+}
+
+
+
+
 
 
 
