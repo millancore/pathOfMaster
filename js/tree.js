3f2ef2c8aab5cfdfdbfcf27d37d75344b440bb81
Juan Millan <juanes14569@hotmail.com>
2018-11-28 18:33:57 -0500

    add config Simplemde

--- js/tree.js
+++ js/tree.js
@@ -1,12 +1,14 @@
 const loadTemplate = require('./templateLoader');
-const getFormDataAsJSON = require('./getFormDataAsJSON')
+const getFormDataAsJSON = require('./getFormDataAsJSON');
+const simpleConfig = require('./simpleConfig');
 
 var addTree = function () {
     loadTemplate('layout', 'render').then(() =>
         loadTemplate('addTree', 'content')
-    ).then(() => {  
+    ).then(() => {
         treeListener();
-        var simplemded = new SimpleMDE({ element: document.getElementById("treeDestrion")});
+        simpleConfig.element = document.getElementById("treeDestrion");
+        var simplemde = new SimpleMDE(simpleConfig);
     })
 };
 
@@ -17,7 +19,7 @@ var tree = function (treeName) {
                 var template = Handlebars.compile(response.data);
                 var dataTree = JSON.parse(localStorage.getItem(treeName));
                 document.getElementById('content').innerHTML = template(dataTree);
-                printtree(treeName);               
+                printtree(treeName);
             }
         );
     })
@@ -34,9 +36,6 @@ function treeListener() {
         var dataJson = getFormDataAsJSON(formNewTree);
         var obj = JSON.parse(dataJson);
         localStorage.setItem(obj.name, dataJson);
-        
-
-
 
         if (localStorage.getItem("usuarioarbol") == null) {
             var treeArray = [obj.name];
@@ -51,47 +50,43 @@ function treeListener() {
     }, false);
 }
 
-function printtree(treeName){
-    var nodeArray = JSON.parse(localStorage.getItem(treeName+"-node"));
+function printtree(treeName) {
+    var nodeArray = JSON.parse(localStorage.getItem(treeName + "-node"));
     var nodos = SVG("tree-see").size(300, 10000);
     var circley = 50;
     var liney1 = 60;
     var liney2 = 0;
 
     for (x = 0; x < nodeArray.length; x++) {
-        liney2 += 90; 
+        liney2 += 90;
     }
 
     var line = nodos.line(133, liney1, 133, liney2).stroke({ width: 10 });
-    
-    line.fill( '#f09' )
+
+    line.fill('#f09')
 
     for (x = 0; x < nodeArray.length; x++) {
-         var circle = nodos.circle(65).fill('#4e6fc9').move(100, circley);
+        var circle = nodos.circle(65).fill('#4e6fc9').move(100, circley);
 
-    
-    
-    
-            
         liney1 += 190;
         circley += 90;
 
         let i = nodeArray[x]
-        circle.click(function() {
+        circle.click(function () {
             axios.get('templates/nodeDescription.hbs').then(
                 function (response) {
                     var template = Handlebars.compile(response.data);
-                    var dataTree = JSON.parse(localStorage.getItem(treeName+"-node"));
+                    var dataTree = JSON.parse(localStorage.getItem(treeName + "-node"));
                     var nodeName = i.name;
                     var nodeDescription = i.descripcion;
-                    var node = {name: nodeName,description: nodeDescription };
+                    var node = { name: nodeName, description: nodeDescription };
                     console.log(node)
 
                     document.getElementById('content').innerHTML = template(node);
                 }
             );
         })
-    }    
+    }
 }
 
 
